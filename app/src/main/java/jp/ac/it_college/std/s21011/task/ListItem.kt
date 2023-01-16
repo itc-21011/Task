29ee78cd6d8ba79bbe7d2ed5815b5f3e3e2eb894
s21002 <s21002@std.it-college.ac.jp>
2023-01-16 12:15:47 +0900

    簡単な表示だけ

---
+++ app/src/main/java/jp/ac/it_college/std/s21011/task/ListItem.kt
@@ -0,0 +1,6 @@
+package jp.ac.it_college.std.s21011.task
+
+data class ListItem(
+    val id: Long,
+    val title: String,
+)
--- app/src/main/java/jp/ac/it_college/std/s21011/task/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s21011/task/MainActivity.kt
@@ -1,11 +1,21 @@
 package jp.ac.it_college.std.s21011.task
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.listView)
+        val data = listOf(
+            ListItem(1, "メモタイトル1"),
+            ListItem(2, "メモタイトル2"),
+            ListItem(3, "メモタイトル3"),
+        )
+        listView.adapter = CustomListAdapter(this, data,R.layout.list_item)
     }
 }
